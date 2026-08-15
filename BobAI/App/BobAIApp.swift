052bee4e484a8bd9847decc5775467b802d16d90
boisey9 <erikboisvert9@gmail.com>
2026-08-15 13:00:32 -0400

    Add debug launch trace for BobAI

--- BobAI/App/BobAIApp.swift
+++ BobAI/App/BobAIApp.swift
@@ -2,6 +2,12 @@ import SwiftUI
 
 @main
 struct BobAIApp: App {
+    init() {
+        #if DEBUG
+        print("[BobAI] App initialized")
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             HomeView()
