9087dcf8edce674440b35aa7c1be4bfbe2d5431d
boisey9 <erikboisvert9@gmail.com>
2026-08-24 07:51:45 -0400

    Enable read-only web credential at Vercel entrypoint

--- Core/src/index.ts
+++ Core/src/index.ts
@@ -1,7 +1,11 @@
 import { loadConfig } from "./config.js";
 import { createBobCoreRuntime } from "./runtime.js";
+import { createReadCredentialGateway } from "./security/read-credential.js";
 
 const config = loadConfig();
 const { app } = createBobCoreRuntime(config);
+const fetch = createReadCredentialGateway(app.fetch, config);
 
-export default app;
+export default {
+  fetch,
+};
